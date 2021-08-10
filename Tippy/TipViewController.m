8e6926fccafda620a8e8080d3b98f63213e336f1
Sarah Wang <sarah.wang2009@gmail.com>
2021-08-10 09:58:16 -0400

    code style fixes

--- Tippy/TipViewController.m
+++ Tippy/TipViewController.m
@@ -20,18 +20,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     [self hideLabels];
-    // Do any additional setup after loading the view.
 }
 
 - (IBAction)onTap:(id)sender {
-    NSLog(@"Hello");
-    
-    //removes keyboard from screen
     [self.view endEditing:true];
 }
 
-//If previous value is 0, then the screen previously had no bill amount inputted (and the labels are hidden). Thus, once the code detects that an amount is being typed in, labels will show.
 int previous = 0;
 
 - (IBAction)updateLabels:(id)sender {
@@ -45,21 +41,18 @@ int previous = 0;
         [self showLabels];
         
     }
-    //an array to hold possible tip percentages
+   
     double tipPercentages[] = {0.15, 0.2, 0.25};
     double tipPercentage = tipPercentages[self.tipPercentageControl.selectedSegmentIndex];
     
-    //calculate tip
     double bill = [self.billField.text doubleValue];
     double tip = bill * tipPercentage;
     double total = bill + tip;
     
-    //update labels
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
 
-//animation to hide calculations and labels
 - (void)hideLabels {
     [UIView animateWithDuration:0.5 animations:^{
         UIColor *colour = [[UIColor alloc]initWithRed:200.0/255.0 green:215.0/255.0 blue:255.0/255.0 alpha:1.0];
@@ -75,12 +68,10 @@ int previous = 0;
         
         self.labelsContainerView.frame = labelsFrame;
         
-        //disappearing labels
         self.labelsContainerView.alpha = 0;
     }];
 }
 
-//animation to hide calculations and labels
 - (void)showLabels {
     [UIView animateWithDuration:0.5 animations:^{
         UIColor *colour = [[UIColor alloc]initWithRed:200.0/255.0 green:255.0/255.0 blue:220/255.0 alpha:1.0];
@@ -96,18 +87,8 @@ int previous = 0;
         
         self.labelsContainerView.frame = labelsFrame;
         
-        //reappearing labels
         self.labelsContainerView.alpha = 1;
     }];
 }
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
